0b20112623db526cb1421a30134d8591c706bcbb
Kenneth Felix <kennf03@gmail.com>
2022-03-30 15:31:41 -0400

    fixes+standup

--- api/main.py
+++ api/main.py
@@ -1,16 +1,24 @@
 from flask import Flask, jsonify, request
 
-app = Flask(__name__)
+from dal.expense_dal.expense_dao_imp import ExpenseDAOImp
+from data_access_layer.employee_data_access.employee_dao_impl import EmployeeDAOImp
+from service.employee_service.employee_service_imp import EmployeeServiceImp
 
+app = Flask(__name__)
+employee_dao = EmployeeDAOImp()
+employee_service = EmployeeServiceImp(employee_dao)
+expense_dao = ExpenseDAOImp()
+expense_service = ExpenseServiceImp(expense_dao)
 
 @app.route("/api/employee", methods=["GET"])
 def employee_login():
     pass
 
 
+# this is mine to write
 @app.route("/api/expense", methods=["POST"])
 def create_expense():
-    pass
+    data : dict = request.get_json()
 
 
 @app.route("/api/expense/<expense_id>", methods=["DELETE"])
--- service/employee_service/employee_service_imp.py
+++ service/employee_service/employee_service_imp.py
@@ -1,8 +1,17 @@
+from custom_exception.bad_input import BadInput
+from data_access_layer.employee_data_access.employee_dao_impl import EmployeeDAOImp
 from entities.employee import Employee
 from service.employee_service.employee_service_interface import EmployeeServiceInterface
 
 
 class EmployeeServiceImp(EmployeeServiceInterface):
 
+    def __init__(self, employee_dao: EmployeeDAOImp):
+        self.employee_dao = employee_dao
+
     def service_employee_login(self, username, password) -> Employee:
-        pass
+        if type(username) is not str:
+            raise BadInput("Wrong type of input entered for username or password")
+        if type(password) is not str:
+            raise BadInput("Wrong type of input entered for username or password")
+        return self.employee_dao.dao_employee_login(username, password)
